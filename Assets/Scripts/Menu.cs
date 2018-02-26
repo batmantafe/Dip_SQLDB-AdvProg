93199d64f59224388d2a396c2a8d0dd2fd59fdeb
batmantafe <duane.reymond@gmail.com>
2018-02-26 11:53:59 +1100

    Added Background, and Login gameobjects, and Menu script.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject loginScreen, signUpScreen, forgotPasswordScreen;
+
+    // Use this for initialization
+    void Start()
+    {
+        LoginButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoginButton()
+    {
+        loginScreen.SetActive(true);
+        signUpScreen.SetActive(false);
+        forgotPasswordScreen.SetActive(false);
+    }
+
+    public void SignUpButton()
+    {
+        loginScreen.SetActive(false);
+        signUpScreen.SetActive(true);
+        forgotPasswordScreen.SetActive(false);
+    }
+
+    public void ForgotPasswordScreen()
+    {
+        loginScreen.SetActive(false);
+        signUpScreen.SetActive(false);
+        forgotPasswordScreen.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        loginScreen.SetActive(true);
+        signUpScreen.SetActive(false);
+        forgotPasswordScreen.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
