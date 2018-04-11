fb0c0c61bb1dd6f4c37e0bd8d3745ca4507a653d
Duane Reymond <duane.reymond@gmail.com>
2018-04-11 14:04:20 +1000

    Edited Login script to work with new Login screen setup.

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class Login : MonoBehaviour
 {
-    public GameObject loginScreen, signUpScreen, forgotPasswordScreen;
+    public Text screen;
+    public GameObject username, email, password, login, signup, forgotpassword, exit, send, confirm, back;
 
     // Use this for initialization
     void Start()
@@ -20,33 +23,74 @@ public class Login : MonoBehaviour
 
     public void LoginButton()
     {
-        loginScreen.SetActive(true);
-        signUpScreen.SetActive(false);
-        forgotPasswordScreen.SetActive(false);
+        screen.text = "Login";
+
+        username.SetActive(true);
+        username.gameObject.GetComponent<InputField>().text = "";
+
+        email.SetActive(false);
+        email.gameObject.GetComponent<InputField>().text = "";
+
+        password.SetActive(true);
+        password.gameObject.GetComponent<InputField>().text = "";
+
+        login.SetActive(true);
+        signup.SetActive(true);
+        forgotpassword.SetActive(true);
+        exit.SetActive(true);
+
+        send.SetActive(false);
+        confirm.SetActive(false);
+        back.SetActive(false);
     }
 
     public void SignUpButton()
     {
-        loginScreen.SetActive(false);
-        signUpScreen.SetActive(true);
-        forgotPasswordScreen.SetActive(false);
+        screen.text = "Sign Up";
+
+        username.SetActive(true);
+        username.gameObject.GetComponent<InputField>().text = "";
+
+        email.SetActive(true);
+        email.gameObject.GetComponent<InputField>().text = "";
+
+        password.SetActive(true);
+        password.gameObject.GetComponent<InputField>().text = "";
+
+        login.SetActive(false);
+        signup.SetActive(false);
+        forgotpassword.SetActive(false);
+        exit.SetActive(false);
+
+        send.SetActive(false);
+        confirm.SetActive(true);
+        back.SetActive(true);
     }
 
     public void ForgotPasswordScreen()
     {
-        loginScreen.SetActive(false);
-        signUpScreen.SetActive(false);
-        forgotPasswordScreen.SetActive(true);
+        screen.text = "Forgot Password";
+
+        username.SetActive(false);
+        username.gameObject.GetComponent<InputField>().text = "";
+
+        email.SetActive(true);
+        email.gameObject.GetComponent<InputField>().text = "";
+
+        password.SetActive(false);
+        password.gameObject.GetComponent<InputField>().text = "";
+
+        login.SetActive(false);
+        signup.SetActive(false);
+        forgotpassword.SetActive(false);
+        exit.SetActive(false);
+
+        send.SetActive(true);
+        confirm.SetActive(false);
+        back.SetActive(true);
     }
 
-    public void BackButton()
-    {
-        loginScreen.SetActive(true);
-        signUpScreen.SetActive(false);
-        forgotPasswordScreen.SetActive(false);
-    }
-
-    public void QuitButton()
+    public void ExitButton()
     {
         Application.Quit();
     }
