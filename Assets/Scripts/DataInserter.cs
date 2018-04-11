38a65b8efddd0c5e2beb163edf4806d46b4adb0c
Duane Reymond <duane.reymond@gmail.com>
2018-04-11 19:11:47 +1000

    Added code to DataInserter script for sending Email to email address entered.

--- Assets/Scripts/DataInserter.cs
+++ Assets/Scripts/DataInserter.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 using UnityEngine.UI;
 
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
 public class DataInserter : MonoBehaviour
 {
     public InputField inputUsername, inputEmail, inputPassword;
 
     public Text debug;
 
-    string CreateUserURL = "localhost/ninja/InsertUser.php";
-    string LoginURL = "localhost/ninja/Login.php";
+    private string CreateUserURL = "localhost/ninja/InsertUser.php";
+    private string LoginURL = "localhost/ninja/Login.php";
 
     // Use this for initialization
     void Start()
@@ -22,7 +28,7 @@ public class DataInserter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     #region Sign Up
@@ -71,6 +77,36 @@ public class DataInserter : MonoBehaviour
     public void Login_Button()
     {
         StartCoroutine(LoginUser(inputUsername.text, inputPassword.text));
+    }
+    #endregion
+
+    #region Email
+    public void EmailUserButton()
+    {
+        debug.text = "";
+
+        MailMessage mail = new MailMessage();
+
+                                // Email
+        mail.From = new MailAddress("");
+        mail.To.Add(inputEmail.text);
+        mail.Subject = "Reset Your 99 NINJA Password";
+        mail.Body = "Hi User, you can reset your Password HERE:";
+
+        //Simple Mail Transfer Protocol
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 25;
+                                                    // Email, Password
+        smtpServer.Credentials = new NetworkCredential("", "") as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+
+        ServicePointManager.ServerCertificateValidationCallback = delegate
+        (object s, X509Certificate cert, X509Chain chain, SslPolicyErrors policyErrors)
+        { return true; };
+
+        smtpServer.Send(mail);
+
+        debug.text = "The Password-Reset Email has been sent to: " + inputEmail.text;
     }
     #endregion
 }
