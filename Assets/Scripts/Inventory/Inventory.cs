e80de0f5eb93c22b9590a9de4167a9dd2de82a7e
Duane Reymond <duane.reymond@gmail.com>
2018-05-17 19:53:36 +1000

    Created Shop gameobject. Enable/Disable Mouselook and Movement scripts on Player when Inventory toggled.

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [Header("Bools")]
     public bool showInv;
+    public bool playerAtShop;
+    public bool playerAtChest;
+
+    public MouseLook playerHeadLook;
+    public MouseLook playerTurn;
+    public Movement playerMove;
 
     // Use this for initialization
     void Start()
     {
-
+        GetStuff();
     }
 
     // Update is called once per frame
@@ -21,29 +28,14 @@ public class Inventory : MonoBehaviour
         }
     }
 
-    private void OnGUI()
+    void OnGUI()
     {
         float scrW = Screen.width / 16;
         float scrH = Screen.height / 9;
 
         if (showInv)
         {
-            // fullscreen Inventory background
-            //GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "INVENTORY");
-
-            GUI.Box(new Rect(scrW * 4.5f, scrH * 1f, scrW * 1f, scrH * 5f), "Equipped");
-
-            GUI.Box(new Rect(scrW * 1f, scrH * 2f, scrW * 3f, scrH * 4f), "Character View");
-
-            GUI.Box(new Rect(scrW * 6f, scrH * 1f, scrW * 5f, scrH * 5f), "Inventory");
-
-            GUI.Box(new Rect(scrW * 6f, scrH * 6.5f, scrW * 5f, scrH * 1f), "Quick Select");
-
-            GUI.Box(new Rect(scrW * 12f, scrH * 1f, scrW * 3f, scrH * 3f), "Shop");
-
-            GUI.Box(new Rect(scrW * 12f, scrH * 5f, scrW * 3f, scrH * 3f), "Chest");
-
-
+            DrawInventories();
         }
     }
 
@@ -57,6 +49,10 @@ public class Inventory : MonoBehaviour
             Time.timeScale = 1;
             Cursor.visible = false;
 
+            playerHeadLook.enabled = true;
+            playerTurn.enabled = true;
+            playerMove.enabled = true;
+
             return (false);
         }
 
@@ -68,7 +64,63 @@ public class Inventory : MonoBehaviour
             Time.timeScale = 0.25f;
             Cursor.visible = true;
 
+            playerHeadLook.enabled = false;
+            playerTurn.enabled = false;
+            playerMove.enabled = false;
+
             return (true);
         }
     }
+
+    void DrawInventories()
+    {
+        float scrW = Screen.width / 16;
+        float scrH = Screen.height / 9;
+
+        // fullscreen Inventory background
+        //GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "INVENTORY");
+
+        GUI.Box(new Rect(scrW * 4.5f, scrH * 1f, scrW * 1f, scrH * 5f), "Equipped");
+
+        GUI.Box(new Rect(scrW * 1f, scrH * 2f, scrW * 3f, scrH * 4f), "Character View");
+
+        GUI.Box(new Rect(scrW * 6f, scrH * 1f, scrW * 5f, scrH * 5f), "Inventory");
+
+        GUI.Box(new Rect(scrW * 6f, scrH * 6.5f, scrW * 5f, scrH * 1f), "Quick Select");
+
+        if (playerAtShop == true)
+        {
+            GUI.Box(new Rect(scrW * 12f, scrH * 1f, scrW * 3f, scrH * 3f), "Shop");
+        }
+
+        if (playerAtChest == true)
+        {
+            GUI.Box(new Rect(scrW * 12f, scrH * 4.5f, scrW * 3f, scrH * 3f), "Chest");
+        }
+    }
+
+    void GetStuff()
+    {
+        playerHeadLook = transform.Find("Head").GetComponent<MouseLook>();
+        playerTurn = GetComponent<MouseLook>();
+        playerMove = GetComponent<Movement>();
+    }
+
+    #region Triggers
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Shop"))
+        {
+            playerAtShop = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (other.gameObject.CompareTag("Shop"))
+        {
+            playerAtShop = false;
+        }
+    }
+    #endregion
 }
