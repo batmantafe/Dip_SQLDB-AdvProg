b9e853f8006a80b0ad604480dc95be1d36acbbc9
Duane Reymond <duane.reymond@gmail.com>
2018-05-18 13:51:05 +1000

    Chest gameobject is now a Prefab. Chest script also determines active cans in gameobject with ChestMaxItems slider range set.

--- Assets/Scripts/Chests/Chest.cs
+++ Assets/Scripts/Chests/Chest.cs
@@ -7,15 +7,26 @@ public class Chest : MonoBehaviour
     [Range(1,9)]
     public int chestMaxItems;
 
+    [Header("Arrays")]
+    public GameObject[] cans;
+
     // Use this for initialization
     void Start()
     {
-
+        SetCans();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void SetCans()
+    {
+        for (int can = 0; can < chestMaxItems; can++)
+        {
+            cans[can].SetActive(true);
+        }
     }
 }
