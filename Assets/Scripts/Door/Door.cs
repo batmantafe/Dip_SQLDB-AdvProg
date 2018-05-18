9edb8aea036834ff184d90cc4c78541d6822d215
Duane Reymond <duane.reymond@gmail.com>
2018-05-18 12:20:25 +1000

    Door at Shop open/closing for Player.

---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject rightDoor, leftDoor;
+
+    public Transform rightPos1, rightPos2, leftPos1, leftPos2;
+
+    public bool playerAtDoor;
+
+    public float doorSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAtDoor = false;
+        doorSpeed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerAtDoor == true)
+        {
+            rightDoor.transform.position = Vector3.MoveTowards(rightDoor.transform.position, rightPos2.position, doorSpeed * Time.deltaTime);
+
+            leftDoor.transform.position = Vector3.MoveTowards(leftDoor.transform.position, leftPos2.position, doorSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            rightDoor.transform.position = Vector3.MoveTowards(rightDoor.transform.position, rightPos1.position, doorSpeed * Time.deltaTime);
+
+            leftDoor.transform.position = Vector3.MoveTowards(leftDoor.transform.position, leftPos1.position, doorSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerAtDoor = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerAtDoor = false;
+        }
+    }
+}
