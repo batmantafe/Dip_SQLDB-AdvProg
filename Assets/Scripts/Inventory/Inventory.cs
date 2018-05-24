795282b3db1fa3c969c0634d1b9ba6eb89d4dcdb
Duane Reymond <duane.reymond@gmail.com>
2018-05-24 15:59:20 +1000

    Started Inventory.

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -27,6 +27,13 @@ public class Inventory : MonoBehaviour
     public GameObject miniMap;
     public GameObject charView;
 
+    [Header("Cert IV Inventory")]
+    //the toggle for showing our inventory
+    //public bool showInv;
+    public List<Items> inventory = new List<Items>();
+    public int slotX, slotY;
+    private Rect inventorySize;
+
     // Use this for initialization
     void Start()
     {
@@ -76,6 +83,21 @@ public class Inventory : MonoBehaviour
             }
         }
     }
+
+    #region AddItem
+    public void AddItem(int iD)
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (inventory[i].name == null)
+            {
+                inventory[i] = ItemGen.CreateItem(iD);
+                Debug.Log("Added Item: " + inventory[i].name);
+                return;
+            }
+        }
+    }
+    #endregion
 
     #region Inventory
     public bool ToggleInv()
@@ -136,6 +158,109 @@ public class Inventory : MonoBehaviour
         GUI.Box(new Rect(scrW * 6f, scrH * 1f, scrW * 5f, scrH * 5f), "Inventory");
 
         GUI.Box(new Rect(scrW * 6f, scrH * 6.5f, scrW * 5f, scrH * 1f), "Quick Select");
+
+        /***********************************/
+
+        #region DragInventory from Cert IV
+
+        //void InventoryDrag(int windowID)
+    //{
+            //GUI.Box(new Rect(0, 0.25f * scrH, 6 * scrW, 0.5f * scrH), "Banner");
+            //GUI.Box(new Rect(0, 4.25f * scrH, 6 * scrW, 0.5f * scrH), "Gold n EXP");
+            //showToolTip = false;
+            #region Nested For Loop
+            Event e = Event.current;
+            int i = 0;
+            for (int y = 0; y < slotY; y++)
+            {
+                for (int x = 0; x < slotX; x++)
+                {
+                    Rect slotLocation = new Rect(scrW * 0.125f + x * (scrW * 0.75f), scrH * 0.75f + y * (scrH * 0.65f), 0.75f * scrW, 0.65f * scrH);
+                    GUI.Box(slotLocation, "");
+
+                    #region Pickup Item
+                    /*
+                    if (e.button == 0 && e.type == EventType.MouseDown && slotLocation.Contains(e.mousePosition) && !dragging && inventory[i].Name != null && !Input.GetKey(KeyCode.LeftShift))
+                    {
+                        draggedItem = inventory[i];//we pick up item
+                        inventory[i] = new Item();//the slot item is now blank
+                        dragging = true;//we are holding an item
+                        draggedFrom = i;//this is so we know where the item came from
+                        Debug.Log("Dragging: "+ draggedItem.Name);
+                    }
+                    #endregion
+                    #region Swap Item
+                    if (e.button == 0 && e.type == EventType.MouseUp && slotLocation.Contains(e.mousePosition) && dragging && inventory[i].Name != null)
+                    {
+                        Debug.Log("Swapping: " + draggedItem.Name + " :With: " + inventory[i].Name);
+
+                        inventory[draggedFrom] = inventory[i];//our pick up slot now has our other item
+                        inventory[i] = draggedItem;//the slot we are dropping the Item is now filled with our drag item
+                        draggedItem = new Item();//the item we use to be dragging is empty
+                        dragging = false;//we are not holding an item
+
+                    }
+                    #endregion
+                    #region Place Item
+                    if (e.button == 0 && e.type == EventType.MouseUp && slotLocation.Contains(e.mousePosition) && dragging && inventory[i].Name == null)
+                    {
+                        Debug.Log("Place: " + draggedItem.Name + " :Into: " + i);
+
+                        inventory[i] = draggedItem;//the slot we are dropping the Item is now filled with our drag item
+                        draggedItem = new Item();//the item we use to be dragging is empty
+                        dragging = false;//we are not holding an item
+
+                    }
+                    #endregion
+                    #region Return Item
+                    if (e.button == 0 && e.type == EventType.MouseUp && i == ((slotX*slotY)-1) && dragging)
+                    {
+                        Debug.Log("Return: " + draggedItem.Name + " :Into: " + draggedFrom);
+
+                        inventory[draggedFrom] = draggedItem;//the slot we are dropping the Item is now filled with our drag item
+                        draggedItem = new Item();//the item we use to be dragging is empty
+                        dragging = false;//we are not holding an item
+
+                    }
+                    */
+                    #endregion
+
+
+                    #region Draw Item Icon
+                    if (inventory[i].name != null)
+                    {
+                        GUI.DrawTexture(slotLocation, inventory[i].iconName);
+
+                        Debug.Log("Draw Item Icon");
+
+                        #region Set ToolTip on Mouse Hover
+                        if (slotLocation.Contains(e.mousePosition) /*&& !dragging*/ && showInv)
+                        {
+                            //toolTipItem = i;
+                            //showToolTip = true;
+                        }
+                        #endregion
+                    }
+                    #endregion
+                    i++;
+                }
+            }
+            #endregion
+            
+            
+            #region Drag Window
+            /*
+            GUI.DragWindow(new Rect(0 * scrW, 0 * scrH, 6 * scrW, 0.5f * scrH));//Top Drag
+            GUI.DragWindow(new Rect(0 * scrW, 0.5f * scrH, 0.25f * scrW, 3.5f * scrH));//Left Drag
+            GUI.DragWindow(new Rect(5.5f * scrW, 0.5f * scrH, 0.25f * scrW, 3.5f * scrH));//Right Drag
+            GUI.DragWindow(new Rect(0 * scrW, 4 * scrH, 0.25f * scrW, 3.5f * scrH));//Bottom Drag
+            */
+            #endregion
+        //}
+
+        #endregion
+
+        /***********************************/
 
         if (playerAtShop == true)
         {
