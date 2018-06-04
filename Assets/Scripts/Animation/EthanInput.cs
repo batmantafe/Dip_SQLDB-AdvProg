27bfa039523623d72f10df337e52e258dd93f42f
Duane Reymond <duane.reymond@gmail.com>
2018-06-04 13:20:14 +1000

    Added Ethan model and animation. Created new Player prefab.

---
+++ Assets/Scripts/Animation/EthanInput.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanInput : MonoBehaviour
+{
+    public float ethanMoveHorizontal;
+    public float ethanMoveFwd;
+    public Animator ethanAnim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EthanInputFunction();
+    }
+
+    void EthanInputFunction()
+    {
+        // Horizontal Movement
+        ethanMoveHorizontal = Input.GetAxis("Horizontal");
+        ethanMoveFwd = Input.GetAxis("Vertical");
+
+        ethanAnim.SetFloat("EForward", ethanMoveFwd);
+        ethanAnim.SetFloat("EHorizontal", ethanMoveHorizontal);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            ethanAnim.SetBool("isRunning", true);
+        }
+        else
+        {
+            ethanAnim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (ethanAnim.GetBool("isCrouching"))
+            {
+                ethanAnim.SetBool("isCrouching", false);
+            }
+            else
+            {
+                ethanAnim.SetBool("isCrouching", true);
+            }
+        }
+    }
+}
