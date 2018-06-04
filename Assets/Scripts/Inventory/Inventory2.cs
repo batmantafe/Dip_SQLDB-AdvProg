83fe63d3be5623f57f9af313f3d79f8da47563a7
Duane Reymond <duane.reymond@gmail.com>
2018-06-04 10:14:16 +1000

    Trying to use Canvas for Inventory, with new Inventory2 script.

---
+++ Assets/Scripts/Inventory/Inventory2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory2 : MonoBehaviour
+{
+    public GameObject playerInventory, miniMap;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartConditions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InventoryInput();
+    }
+
+    void StartConditions()
+    {
+        playerInventory.SetActive(false);
+        miniMap.SetActive(true);
+    }
+
+    void InventoryInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (playerInventory.activeSelf == false)
+            {
+                playerInventory.SetActive(true);
+
+                miniMap.SetActive(false);
+            }
+            else
+            {
+                playerInventory.SetActive(false);
+
+                miniMap.SetActive(true);
+            }
+
+        }
+    }
+}
