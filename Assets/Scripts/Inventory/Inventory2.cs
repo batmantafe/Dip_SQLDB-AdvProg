1eaad460b6898c4569995fc5ef14f17c22972dce
Duane Reymond <duane.reymond@gmail.com>
2018-06-04 10:44:43 +1000

    New ItemButton script that goes on Inventory buttons.

--- Assets/Scripts/Inventory/Inventory2.cs
+++ Assets/Scripts/Inventory/Inventory2.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class Inventory2 : MonoBehaviour
 {
     public GameObject playerInventory, miniMap;
+
+    public MouseLook playerXLook, playerYLook;
+    public Movement playerMovement;
+
+    public GameObject[] itemButtons;
+
+    public int itemNumber;
 
     // Use this for initialization
     void Start()
@@ -22,25 +31,50 @@ public class Inventory2 : MonoBehaviour
     {
         playerInventory.SetActive(false);
         miniMap.SetActive(true);
+        Cursor.visible = false;
     }
 
     void InventoryInput()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
+            // Turn Inventory ON!
             if (playerInventory.activeSelf == false)
             {
                 playerInventory.SetActive(true);
+                Cursor.visible = true;
 
                 miniMap.SetActive(false);
+                playerXLook.enabled = false;
+                playerYLook.enabled = false;
+                playerMovement.enabled = false;
+
+                CheckInventoryButtons();
             }
+
+            // Turn Inventory OFF!
             else
             {
                 playerInventory.SetActive(false);
+                Cursor.visible = false;
 
                 miniMap.SetActive(true);
+                playerXLook.enabled = true;
+                playerYLook.enabled = true;
+                playerMovement.enabled = true;
             }
 
+        }
+    }
+
+    void CheckInventoryButtons()
+    {
+        for (int i = 0; i < itemButtons.Length; i++)
+        {
+            if (itemButtons[i].GetComponent<ItemButton>().itemPresent == false)
+            {
+                itemButtons[i].SetActive(false);
+            }
         }
     }
 }
