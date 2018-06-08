ecd7befa027916f458b3699590966f032a355d23
Duane Reymond <duane.reymond@gmail.com>
2018-06-08 13:59:04 +1000

    Got SQL_As2 working a bit with SQL, getting CharacterName and Class from correct Username in database.

---
+++ Assets/Scripts/SQL_As3/HUD.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace sql3
+{
+
+    public class HUD : MonoBehaviour
+    {
+        public bool hudCheckInvOn;
+
+        [Header("Health")]
+        public float healthMax;
+        public float healthCurrent;
+        public GUIStyle healthGui;
+
+        [Header("Mana")]
+        public float manaMax;
+        public float manaCurrent;
+        public GUIStyle manaGui;
+
+        [Header("Stamina")]
+        public float stamMax;
+        public float stamCurrent;
+        public GUIStyle stamGui;
+        public float stamBurn;
+        public float stamRegen;
+        public float playerNormalSpeed;
+        public float playerSlowSpeed;
+
+        // Use this for initialization
+        void Start()
+        {
+            StartConditions();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            StaminaUse();
+        }
+
+        void StartConditions()
+        {
+            // Set Health
+            healthMax = 100f;
+            healthCurrent = healthMax;
+
+            manaMax = 100f;
+            manaCurrent = manaMax;
+
+            stamMax = 100f;
+            stamCurrent = stamMax;
+            stamBurn = 10f;
+            stamRegen = 5f;
+            playerNormalSpeed = GetComponent<Movement>().speed;
+            playerSlowSpeed = 1f;
+        }
+
+        void OnGUI()
+        {
+            float scrW = Screen.width / 16;
+            float scrH = Screen.height / 9;
+
+            if (hudCheckInvOn == false)
+            {
+                GUI.Box(new Rect(scrW * 6f, scrH * 7f, healthCurrent * (scrW * 4f) / healthMax, scrH * 0.5f), "Health", healthGui);
+
+                GUI.Box(new Rect(scrW * 6f, scrH * 7.5f, manaCurrent * (scrW * 4f) / manaMax, scrH * 0.5f), "Mana", manaGui);
+
+                GUI.Box(new Rect(scrW * 6f, scrH * 8f, stamCurrent * (scrW * 4f) / stamMax, scrH * 0.5f), "Stamina", stamGui);
+            }
+
+            if (hudCheckInvOn == true)
+            {
+                GUI.Box(new Rect(scrW * 1f, scrH * 6.25f, healthCurrent * (scrW * 3f) / healthMax, scrH * 0.25f), "Health", healthGui);
+
+                GUI.Box(new Rect(scrW * 1f, scrH * 6.5f, manaCurrent * (scrW * 3f) / manaMax, scrH * 0.25f), "Mana", manaGui);
+
+                GUI.Box(new Rect(scrW * 1f, scrH * 6.75f, stamCurrent * (scrW * 3f) / stamMax, scrH * 0.25f), "Stamina", stamGui);
+            }
+        }
+
+        void StaminaUse()
+        {
+            if (stamCurrent >= stamMax)
+            {
+                stamCurrent = stamMax;
+            }
+
+            if (stamCurrent <= 0f)
+            {
+                stamCurrent = 0f;
+
+                GetComponent<Movement>().speed = playerSlowSpeed;
+            }
+
+            if (stamCurrent > 0f)
+            {
+                GetComponent<Movement>().speed = playerNormalSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            {
+                stamCurrent = stamCurrent - (stamBurn * Time.deltaTime);
+            }
+
+            else
+            {
+                stamCurrent = stamCurrent + (stamRegen * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SQL_As3/SQL3_Login.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+namespace sql3
+{
+    
+
+    public class SQL3_Login : MonoBehaviour
+    {
+        [Header("Username Login Start Check")]
+        public bool userLoggedIn;
+        public MouseLook mouseLookX, mouseLookY;
+        public Movement movement;
+        public HUD hud;
+
+        [Header("Username SQL Check")]
+        public InputField usernameInput;
+        private string checkUsernameURL = "localhost/ninja/sql3CheckUsername.php";
+
+        // Use this for initialization
+        void Start()
+        {
+            userLoggedIn = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UserNotLoggedIn();
+
+            CheckUsername();
+        }
+
+        #region Is the User Logged In?
+        void UserNotLoggedIn()
+        {
+            if (!userLoggedIn)
+            {
+                mouseLookX.enabled = false;
+                mouseLookY.enabled = false;
+                movement.enabled = false;
+                hud.enabled = false;
+            }
+
+            else
+            {
+                mouseLookX.enabled = true;
+                mouseLookY.enabled = true;
+                movement.enabled = true;
+                hud.enabled = true;
+            }
+        }
+
+        void CheckUsername()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                StartCoroutine(CheckUsernameNow(usernameInput.text));
+            }
+        }
+
+        IEnumerator CheckUsernameNow(string username)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("usernamePost", username);
+
+            WWW www = new WWW(checkUsernameURL, form);
+
+            yield return www;
+
+            // If NO ERROR from SQL
+            if (!www.text.Contains("ERROR"))
+            {
+                Debug.Log(www.text);
+
+                string wwwTextString = www.text;
+            }
+
+            // If ERROR from SQL
+            else
+            {
+                Debug.Log(www.text);
+            }
+        }
+
+        #endregion
+    }
+
+}
